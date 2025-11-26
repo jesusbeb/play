ab1bdf6b21aeb411dd121f26e8a812ad252057d6
jesusbeb <jesusbelb@gmail.com>
2025-11-26 15:02:26 -0600

    Configuracion de application.properties y perfiles en Spring Boot

--- src/main/java/com/jbelt/play/HelloController.java
+++ src/main/java/com/jbelt/play/HelloController.java
@@ -1,15 +1,20 @@
 package com.jbelt.play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //RestController indica que es un controlador de peticiones
 @RestController
 public class HelloController {
+    private final String plataform;
     private final PlayAiService aiService; //constante
 
-    //Constructor
-    public HelloController(PlayAiService aiService) {
+    // Constructor
+    // Value("${}" sirve para inyectar el valor de una variable que esta dentro del
+    // archivo application.properties al parametro String plataform
+    public HelloController(@Value("${spring.application.name}") String plataform, PlayAiService aiService) {
+        this.plataform = plataform;
         this.aiService = aiService;
     }
 
@@ -17,6 +22,6 @@ public class HelloController {
     //GetMapping indica que recibe peticion de tipo get
     @GetMapping("/")
     public String hello(){
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(plataform);
     }
 }
--- src/main/java/com/jbelt/play/PlayAiService.java
+++ src/main/java/com/jbelt/play/PlayAiService.java
@@ -1,16 +1,19 @@
 package com.jbelt.play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 //En esta clase ponemos todo lo perteneciente a ayudas de IA
 @AiService
 public interface PlayAiService {
 
-    //Metodo que envia un mensaje mediante IA
+    // Metodo que envia un mensaje mediante IA
+    // Recibe un String cuyo valor se incluira como parte del prompt que generara la
+    // IA, para lo cual se usa la anotacion @V("nombreVariable")
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Gestion de Peliculas.
+            Genera un saludo de bienvenida a la plataforma de Gestion de Peliculas {{plataform}}.
             Usa menos de 120 caracteres y hazlo con un estilo amistoso.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("plataform") String plataform);
 }
