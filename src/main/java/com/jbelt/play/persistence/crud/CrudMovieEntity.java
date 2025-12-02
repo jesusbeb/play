1ec2ae1764bb1955607b8101df1b2585b986ae57
jesusbeb <jesusbelb@gmail.com>
2025-12-02 12:34:30 -0600

    Repositorio CRUD con Spring Data sin escribir SQL

---
+++ src/main/java/com/jbelt/play/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,10 @@
+package com.jbelt.play.persistence.crud;
+
+import com.jbelt.play.persistence.Entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+// Repositorio que extiende de CrudRepository para tener accceso a todos los metodos para
+// realizar operaciones en la BD sin escribir codigo SQL
+//Recibe dos elementos genericos, nombre del Entity y tipo de dato de su clave primaria
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+}
